5f90381b2ef5e2f3efeea44300d58323f25cc3c2
dk <kbs245677@gmail.com>
2023-09-08 10:15:18 +0900

    myRef

--- src/App.js
+++ src/App.js
@@ -1,48 +1,41 @@
+import { createRef, useRef, useState } from 'react';
 import './App.css';
-import { useMemo, useState } from 'react';
 
+//useRef(디자인)
+//dom을 변경할 때 사용
 function App() {
-  const [list, setList] = useState([1, 2, 3, 4]);
-  const [str, setStr] = useState('합계');
+  const myRef = useRef(null);
+  const [list, setList] = useState([
+    { id: 1, name: '이대호' },
+    { id: 2, name: '최대성' },
+  ]);
 
-  const getAddResult = () => {
-    let sum = 0;
-    list.forEach((i) => (sum = sum + i));
-    console.log('sum 함수 실행', sum);
-    return sum;
-  };
-
-  //리스트 값이 추가 됐을때는 sum 함수 실행이 되는것이 당연하다
-  //그러나 문자가 변경됐을때도 sum 함수가 실행이 되어서는 안된다
-  //이럴때 useMemo를 사용하여 list가 변경되었을때만 sum함수가 실행되도록 한다.
-  //나는 getAddResult()라는 함수를 기억할게 [list]가 변경 됐을때
-  const addResult = useMemo(() => getAddResult(), [list]);
+  //myRef가 배열로 만들어진다. createRef만큼!
+  const myRefs = Array.from({ length: list.length }).map(() => createRef());
 
   return (
     <div>
       <button
         onClick={() => {
-          setStr('바뀐 합계');
+          console.log(myRef);
+          console.log(myRef.current);
+          // myRef.current.style.backgroundColor = 'gray'; box 색깔 변경
+
+          // myRefs[0].current.style.backgroundColor = 'red'; 이대호 색깔 변경
+          myRefs[1].current.style.backgroundColor = 'blue'; // 최대성 색깔 변경
         }}
       >
-        문자 변경
+        색변경
       </button>
-      <button
-        onClick={() => {
-          setList([...list, 10]);
-        }} // 리스트의 값이 바뀔때 다시 리턴이 되어서
-      >
-        리스트 값 추가
-      </button>
+      <div ref={myRefs}>box</div>
       <div>
-        {list.map((i) => (
-          <h1>{i}</h1>
+        롯데자이언츠 선수 :{' '}
+        {list.map((user, index) => (
+          <h1 ref={myRefs[index]}>{user.name}</h1>
         ))}
+        {/* 근데 myRef를 붙이면 최대성 하나밖에 적용이안된다 */}
+        {/* myRefs는 user에 index를 부여해 응답해준다. 그래서 동적으로 실행되게 만들어준다. */}
       </div>
-      <div>
-        {str} : {addResult}
-      </div>
-      {/* 얘의 값변화에도 영항을 준다. */}
     </div>
   );
 }
